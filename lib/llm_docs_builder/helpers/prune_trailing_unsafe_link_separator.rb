4626982d0a150256c6de031b8ae4a389bf7c0d75
Eric-Guo <eric.guocz@gmail.com>
2025-11-05 20:56:36 +0800

    ✨Simplify the file lib/llm_docs_builder/helpers/prune_trailing_unsafe_link_separator.rb, should be more ruby style.

--- lib/llm_docs_builder/helpers/prune_trailing_unsafe_link_separator.rb
+++ lib/llm_docs_builder/helpers/prune_trailing_unsafe_link_separator.rb
@@ -3,30 +3,19 @@
 module LlmDocsBuilder
   module Helpers
     def prune_trailing_unsafe_link_separator!(parts)
-      return if parts.empty?
-
-      loop do
-        break if parts.empty?
-
+      while parts.any?
         last = parts.last
         new_last = last.sub(/[ \t]*\|\s*\z/, '')
 
         if new_last != last
           trimmed = new_last.rstrip
-          if trimmed.empty?
-            parts.pop
-          else
-            parts[-1] = trimmed
-          end
-          next
-        end
-
-        if last.strip.empty?
+          parts[-1] = trimmed
+          parts.pop if trimmed.empty?
+        elsif last.strip.empty?
           parts.pop
-          next
+        else
+          break
         end
-
-        break
       end
     end
 
